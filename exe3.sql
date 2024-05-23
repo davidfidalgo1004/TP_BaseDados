478c11373ebd1f9af58b746ae0741576ab2b3877
David Fidalgo <160844915+davidfidalgo1004@users.noreply.github.com>
2024-05-23 22:44:21 +0100

    Create exe3.sql

---
+++ exe3.sql
@@ -0,0 +1,25 @@
+USE Pessoas
+Go
+
+CREATE PROCEDURE formandoporformador @cc_formador BIGINT, @valortotal FLOAT OUTPUT
+AS
+	BEGIN
+		DECLARE @data_hoje DATE
+		DECLARE @data_ano_atras DATE
+		SET @data_hoje = GETDATE()
+		SET @data_ano_atras = DATEADD(YEAR, -1, @data_hoje)
+		SELECT COUNT(DISTINCT ff.CC_Formandos) AS Quantidade_Alunos
+		FROM Formadores F, Formacao ff
+		WHERE f.CC_Formadores = @cc_formador AND
+			  f.CC_Formadores= ff.CC_Formadores AND
+			  ff.Data_formacao BETWEEN @data_ano_atras AND @data_hoje
+		SELECT @valortotal=SUM(ff.preco)
+		FROM Formadores f, Formacao ff
+		WHERE f.CC_Formadores = @cc_formador AND
+			  f.CC_Formadores= ff.CC_Formadores AND
+			  ff.Data_formacao BETWEEN @data_ano_atras AND @data_hoje
+	END
+
+DECLARE @valor FLOAT
+EXECUTE formandoporformador 14111111149, @valor OUTPUT
+SELECT @valor AS ValorTotalPago
