8f197a388b0ff9303582a5d802288fa4b2299acf
David Fidalgo <160844915+davidfidalgo1004@users.noreply.github.com>
2024-06-06 15:46:46 +0100

    Update and rename exe3.sql to exe3final.sql

--- exe3final.sql
+++ exe3final.sql
@@ -2,6 +2,7 @@ USE Pessoas
 Go
 
 -----------------------------------EXERCICIO 3-----------------------------------------------------------------------------
+-----------------------------------EXERCICIO 3-----------------------------------------------------------------------------
 CREATE PROCEDURE formandoporformador @cc_formador BIGINT, @valortotal FLOAT OUTPUT
 AS
 	BEGIN
@@ -24,4 +25,32 @@ AS
 
 DECLARE @valor FLOAT
 EXECUTE formandoporformador 14111111149, @valor OUTPUT
+SELECT @valor AS ValorTotalPago
+
+--
+
+CREATE PROCEDURE formandoporformador2 @cc_formador BIGINT
+AS
+	BEGIN
+		
+		DECLARE @data_hoje DATE
+		DECLARE @data_ano_atras DATE
+		SET @data_hoje = GETDATE()
+		SET @data_ano_atras = DATEADD(YEAR, -1, @data_hoje)
+
+		SELECT COUNT(DISTINCT ff.CC_Formandos) AS Quantidade_Alunos
+		FROM Formadores F, Formacao ff
+		WHERE f.CC_Formadores = @cc_formador AND
+			  f.CC_Formadores= ff.CC_Formadores AND
+			  ff.Data_formacao BETWEEN @data_ano_atras AND @data_hoje
+		return (SELECT SUM(ff.preco)
+		FROM Formadores f, Formacao ff
+		WHERE f.CC_Formadores = @cc_formador AND
+			  f.CC_Formadores= ff.CC_Formadores AND
+			  ff.Data_formacao BETWEEN @data_ano_atras AND @data_hoje)
+		
+	END
+
+DECLARE @valor FLOAT
+EXECUTE @valor=formandoporformador2 14111111149
 SELECT @valor AS ValorTotalPago
